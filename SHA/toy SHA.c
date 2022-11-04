add2b17ee1f1cab8ff2b321addd1759b5e838a6a
prateekbashista <bashistaprateek@gmail.com>
2022-11-04 09:19:22 -0400

    Update toy SHA.c

--- SHA/toy SHA.c
+++ SHA/toy SHA.c
@@ -4,13 +4,15 @@
 
 static int elements =0;
 void toySHA(unsigned char Input[] ,int CHUNK, unsigned char SHAkey[]) {
-
+     // This function generates a 20 element hash key. Each element is 1 byte, thus, 20*1*8 = 160 BIT hash is generated
+   // Initialises the hash key to '/' .
    for(int i=0;i<20;i++)
     {
         SHAkey[i] = '/';
  //  printf("%c\n",SHAkey[i]);
     }
 
+   // Assigns a simplified compute of Input to hash array
   for(int i=0,j=0;i<CHUNK,j<20;i++,j++)
     {
         SHAkey[j] = (Input[i]+4)/2;
@@ -21,49 +23,6 @@ void toySHA(unsigned char Input[] ,int CHUNK, unsigned char SHAkey[]) {
 
 }
 
-void dedup(unsigned char SHAkey[], int stat){
-
-  unsigned char record[20][20];
- int flag=0, same=0;
- stat = 0;
-  if(elements ==0 ){
-    for(int i=0;i<1;i++){
-    for(int j=0;j<20;j++){
-      record[i][j] = SHAkey[j];
-      printf("%c\n",record[i][j]);
-  }
-}
-    stat = 1;
-    elements++;
-}
-else{
-
-  for(int i=0;i<elements;i++){
-    same=0 ;
-    flag=0;
-    for(int j=0;j<20;j++){
-      if(record[i][j] == SHAkey[j])
-      {
-        same++;
-      }
-    }
-    if(same==6)
-    {
-      stat = 1;
-      flag =1;
-    }
-  }
-
-  if(flag==0){
-    for(int j=0;j<20;j++)
-      {
-        record[elements][j] == SHAkey[j];
-      }
-  }
-
-
-}
-}
 
 void main() {
 
